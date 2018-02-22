7ee066716ad66b81281566b8f7f320c85950ca7f
Ángel Marín <35677091+SilverKnightAnnt@users.noreply.github.com>
2018-02-21 19:08:35 -0600

    Creación de la clase ProcesadorRomano
    Además se crea el método Convierte, por lo que la prueba #1 pasa el Test

---
+++ PrimerExamenKata/src/main/java/com/cenfotec/patrones/logica/ProcesadorRomano.java
@@ -0,0 +1,15 @@
+package com.cenfotec.patrones.logica;
+
+public class ProcesadorRomano {
+
+	
+	public int Convierte(String numero) {
+		
+		int i = 0;
+		if (numero == "") {
+			i = 0;
+		}
+		
+		return i;
+	}
+}
