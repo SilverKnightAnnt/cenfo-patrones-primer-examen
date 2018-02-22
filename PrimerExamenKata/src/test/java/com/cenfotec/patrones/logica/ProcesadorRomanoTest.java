1eed2199a753789b00fbd2978961a90245332454
Ángel Marín <35677091+SilverKnightAnnt@users.noreply.github.com>
2018-02-21 19:16:11 -0600

    Creación del Test#2

--- PrimerExamenKata/src/test/java/com/cenfotec/patrones/logica/ProcesadorRomanoTest.java
+++ PrimerExamenKata/src/test/java/com/cenfotec/patrones/logica/ProcesadorRomanoTest.java
@@ -11,4 +11,14 @@ public class ProcesadorRomanoTest {
 		ProcesadorRomano miProce = new ProcesadorRomano();
 		assertEquals(0, miProce.Convierte(""));
 	}
+	
+	@Test 
+	public void procesarString() {
+		ProcesadorRomano miProce = new ProcesadorRomano();
+		assertEquals(1, miProce.Convierte("I"));
+		assertEquals(2, miProce.Convierte("II"));
+		assertEquals(3, miProce.Convierte("III"));
+		assertEquals("Error", miProce.Convierte("IIII"));
+	}
+	
 }
